3e63dba3c76c71e5c24fafbe1b49d49d4c7f4978
Shazza <sho174@uclive.ac.nz>
2024-05-27 09:42:35 +1200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -22,10 +22,11 @@ import requests
 import pandas as pd
 from datetime import datetime 
 import streamlit as st
+import matplotlib.pyplot as plt
 
 API_KEY = "273a432a-4c0f-4cad-b1cb-304a90bdc6a1"
 API_URL_HEAD = "https://data.moenv.gov.tw/api/v2/aqx_p_488?language=en&offset=0&limit=1000&api_key="
-SUCCESS_MSG = "Successfully downloaded. Please check your folder with the file ends with '_aqi_data.csv' "
+# SUCCESS_MSG = "Successfully downloaded.\nPlease check your folder with the file ends with '_aqi_data.csv' "
 LIST_SENSORS = ['pm2.5', 'so2', 'o3', 'co', 'pm10', 'no2', 'no']
 
 def call_api():
@@ -41,7 +42,7 @@ def call_api():
     
 
 def available_range(records):
-    '''To check the available time range'''
+    '''To check the available time range, so the dropdown selectbox has options.'''
     dates_dict = {}
     for record in records:
         created_date = record['datacreationdate']
@@ -60,20 +61,19 @@ def available_range(records):
 
 def start_date_time(time_list):
     '''Ask user to set the start datetime within the available range.'''
-    # Create the dropdown list
-    selected_time = st.selectbox("Select start datetime", time_list[:-1])
+    selected_time = st.selectbox("Start datetime:", time_list[:-1])
     return selected_time
 
 
 def end_date_time(start_str, time_list):
-    '''Ask user to set the end day and time in the new range and check the 
-    validity. The end date should be at least one hour after start time'''
+    '''Ask user to set the end datetime in the new range and check the validity. 
+    The end time should be at least one hour after start time.'''
     update_time_list = []
     for time in time_list:
-        if datetime.strptime(time, "%Y-%m-%d %H:%M")> datetime.strptime(start_str, 
+        if datetime.strptime(time, "%Y-%m-%d %H:%M") > datetime.strptime(start_str, 
                                                                         "%Y-%m-%d %H:%M"):
             update_time_list.append(time)
-    selected_time = st.selectbox(f"Select end datetime", update_time_list)
+    selected_time = st.selectbox(f"End datetime:", update_time_list)
     return selected_time
 
 
@@ -86,19 +86,18 @@ def combine_lists(selected_lst):
 
 
 def load_data(cols_list, start_str, end_str):
-    '''Filter attributes and load data to the empty data frame.'''
+    '''First to notify users that the data is loading, then make API get request.
+    Filter attributes and load data to the empty data frame.
+    (For) Loop through the data, extract specific columns, and append them to the DataFrame.
+    Append the filtered dictionary to the list of rows. '''
     data_load_state = st.text(f"Loading data from {start_str} to {end_str}...")
-    # make API call to the Air quality index (AQI)(historical data)
     limit_data = requests.get(API_URL_HEAD + API_KEY
                             + '&filters=datacreationdate,GR,' + start_str + ':00'
                             + '|datacreationdate,LE,' + end_str + ':00').json()
     records_list = limit_data["records"] # records_list is a list of dictionaries
-    # Initialize an empty list to store DataFrame rows
-    rows = []
-    # Loop through the data, extract specific columns, and append them to the DataFrame
+    rows = [] # Initialize an empty list to store DataFrame rows
     for record_dict in records_list: # record_dict is the individual dictionary in the records_list
         filtered_dict = {key: record_dict[key] for key in cols_list} # dictionary comprehension
-        # Append the filtered dictionary to the list of rows
         rows.append(filtered_dict)
     df_records = pd.DataFrame(rows)
     return df_records, data_load_state
@@ -108,7 +107,7 @@ def download_file(df_records, start_str, end_str, data_load_state):
     '''The function will be actived after use click Download button. 
     First, change the format of time so the filename can be shorter. 
     Secondly, save as a csv file with start and end DateTime. 
-    The success message will replace the original loading message.
+    New reminder will replace the original loading message.
     '''
     start_str, end_str = start_str.strip(':00'), end_str.strip(':00')
     csv = df_records.to_csv(index = False)
@@ -118,26 +117,22 @@ def download_file(df_records, start_str, end_str, data_load_state):
         file_name = f'{start_str}_to_{end_str}_aqi_data.csv'
         )
     if st.download_button:
-        data_load_state.text(SUCCESS_MSG)
+        data_load_state.text("Please click button below to download the file!")
 
 
-def preview_df(df_records):
-    '''To show the preview of the files'''
-    st.write('Preview dataframe:')
-    st.dataframe(df_records[:1])
-
-
-def preview_chart(df_records):
+def preview_chart(df_records, selected_lst):
     '''To preview the plotting function provided and hint users that there is another page 
     they can use to print different kinds of chart.'''
-    st.write('Preview chart:')
-    st.line_chart(df_records, x = "datacreationdate", y = df_records[0][5:])
     st.markdown(
         """
         **👈 Select Program 3 from the sidebar to see more charts** 
         (Please ignore the demo pages)
         """
     )
+    st.write(f'Preview chart - {selected_lst[0]} level at site 1 (Keelung):')
+    condition = df_records['siteid'] == '1'
+    filtered_df = df_records[condition] # Apply the filter to the DataFrame
+    st.scatter_chart(filtered_df, x = 'datacreationdate', y = selected_lst[0])
 
 
 def main():
@@ -167,21 +162,25 @@ def main():
     records = call_api()
     st.subheader('Step 1. Select Time')
     time_list = available_range(records)
-    start_str = start_date_time(time_list)
+    col1, col2 = st.columns(2)  # Create two columns
+    with col1:
+        start_str = start_date_time(time_list)
     if start_str:
-        end_str = end_date_time(start_str, time_list)
+        with col2:
+            end_str = end_date_time(start_str, time_list)
     st.subheader('Step 2. Select Sensor(s)/Pollutant(s)') 
-    selected_lst = st.multiselect("Please select at least one. For tutor, please select more "
-                                  "than 3.", LIST_SENSORS, [LIST_SENSORS[0]]) 
-    if len(selected_lst) >= 1 and st.button("Download"):
+    selected_lst = st.multiselect("Please select at least one.", 
+                                  LIST_SENSORS, [LIST_SENSORS[0]]) 
+    if len(selected_lst) >= 1 and st.button("Load"):
         cols_list = combine_lists(selected_lst)
         df_records, data_load_state = load_data(cols_list, start_str, end_str)
-        preview_df(df_records)
+        #preview_df(df_records)
         download_file(df_records, start_str, end_str, data_load_state)
+        preview_chart(df_records, selected_lst)
     elif len(selected_lst) < 1:
         st.warning('Download button will only be shown when there are more than one '
                    'sensor/pollutant selected.', icon="⚠️")
-    preview_chart(df_records)
+    
     
 
 if __name__ == "__main__":
