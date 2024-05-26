996a3a2692defc99f421cd5f6e4a58d5ee0f2489
Shazza <sho174@uclive.ac.nz>
2024-05-27 01:46:43 +1200

    add lines

--- pages/0_Program 2 - Merge Files.py
+++ pages/0_Program 2 - Merge Files.py
@@ -11,11 +11,25 @@ import streamlit as st
 
 def upload():
     '''To let users upload the files they want to merge'''
-    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+    uploaded_files = st.file_uploader("Choose a CSV file", type = 'csv', accept_multiple_files=True)
     for uploaded_file in uploaded_files:
         bytes_data = uploaded_file.read()
         st.write("filename:", uploaded_file.name)
         st.write(bytes_data)
+    return uploaded_files
+
+def show_uploads(uploaded_files):
+    '''To show the preview of the files'''
+    for uploaded_file in uploaded_files:
+        infile = open(uploaded_file)
+        lines = infile.read().splitlines()
+        infile.close()
+        for line in lines[:5]:
+            line = line.split(',')
+            table = []
+            table.append(line)
+            # Display the uploaded file
+            st.table(table)
 
 def main():
     '''The main function includes other functions'''
@@ -29,8 +43,10 @@ def main():
         """This program is mainly to merge multiple csv files with same columns and 
     order of columns since we have limitation on extracting API data."""
     )
-    upload()
+    uploaded_files = upload() # uploaded_files will be a list 
+    if uploaded_files is not None:
+        show_uploads(uploaded_files)
 
-    
+
 if __name__ == "__main__":
     main()
