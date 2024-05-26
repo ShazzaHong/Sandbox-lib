b8f244839363be12896768782d25097789c65117
Shazza <sho174@uclive.ac.nz>
2024-05-27 10:01:18 +1200

    Update 0_Program 2 - Merge Files.py

--- pages/0_Program 2 - Merge Files.py
+++ pages/0_Program 2 - Merge Files.py
@@ -15,49 +15,23 @@ import streamlit as st
 BASIC_COL_SET = set(['siteid', 'sitename', 'datacreationdate', 'aqi', 'status'])
 
 
-def upload():
-    '''To let users upload the files they want to merge'''
-    uploaded_files = st.file_uploader("Choose CSV file(s) you want to merge", 
-                                      type = 'csv', accept_multiple_files = True)
-    return uploaded_files
+class FileHandler:
+    '''This class is mainly to handle files. '''
+    def __init__(self):
+        self.uploaded_files = []
 
+    def upload_files(self):
+        # Upload files
+        self.uploaded_files = st.file_uploader("Upload files", accept_multiple_files=True)
 
-def has_basic_cols(uploaded_files):
-    '''To check if the files have basic columns'''
-    for file in uploaded_files:
-        df = pd.read_csv(file.name)
-        for col in df[0]:
-            if col not in BASIC_COL_SET:
-                st.write(f'{file} is missing basic columns. This program only merges files '
-                         f'with {BASIC_COL_SET} columns. Please delete the file and reupload.')
-                #return False
-    print('done')   
-    
-
-def preview_uploads(uploaded_files):
-    '''To show the preview of the files'''
-    if uploaded_files:
-        dfs = []  # List to store DataFrames read from uploaded files
-        for file in uploaded_files:
-            # file_name = file_name.append(file.name)
-            df = pd.read_csv(file)
-            dfs.append(df)
-    for num in range(len(uploaded_files)):
-        st.write(f'Preview {num + 1}:')
-        st.dataframe(dfs[num][:1])
-
-
-def merge_files(file_list, basic_cols_set): #Ask tutor about RuntimeWarning
-    '''merging the files'''
-    # Initialize merged_df with the first DataFrame
-    merged_df = pd.read_csv(file_list[0])
-    
-    # Iterate over the remaining file paths and merge each DataFrame with merged_df
-    for file in file_list[1:]:
-        df = pd.read_csv(file)
-        # Perform an outer join on the common columns
-        merged_df = pd.merge(merged_df, df, on = BASIC_COL_SET, how = 'outer')
-    return merged_df
+    def process_files(self):
+        for uploaded_file in self.uploaded_files:
+            try:
+                # Attempt to read the uploaded file
+                file_contents = uploaded_file.read()
+                st.write("File contents:", file_contents)
+            except FileNotFoundError:
+                st.error("File not found. Please make sure you uploaded the correct files.")
 
 
 def main():
@@ -68,10 +42,13 @@ def main():
         order of columns since we have limitation on extracting API data."""
                 )
     
-    uploaded_files = upload() # uploaded_files will be a list 
-    if uploaded_files is not None:
-        has_basic_cols(uploaded_files)
-        preview_uploads(uploaded_files)
+    # Create an instance of FileHandler
+    file_handler = FileHandler()
+
+    # Call methods to upload and process files
+    file_handler.upload_files()
+    file_handler.process_files()
+    
         
 
 if __name__ == "__main__":
